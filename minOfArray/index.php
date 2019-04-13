e909a447eedc517988db6195f90f2114f226ff3f
Hieu Nguyen <nguyenthanhhieu2995@gmail.com>
2019-04-13 09:46:21 +0700

    [Bài tập] Tìm giá trị nhỏ nhất trong mảng sử dụng phương thức

---
+++ minOfArray/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2>Tìm phần tử nhỏ nhất của mảng có độ lớn cho trước</h2>
+    <form method='post' action="input.php">
+        <?php echo "<br/>Size of array: <input type='text' name='size'/>" ?>
+        <input type='submit' value='GO' />
+    </form>
+
+</body>
+
+</html>
