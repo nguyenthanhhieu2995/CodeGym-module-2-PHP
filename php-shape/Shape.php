7b88e7f6b760a5eac7251f0235ccc145df64db9e
Hieu Nguyen <nguyenthanhhieu2995@gmail.com>
2019-04-19 15:14:47 +0700

    php-shape.

---
+++ php-shape/Shape.php
@@ -0,0 +1,15 @@
+<?php
+include_once("Resizeable.php");
+class Shape
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function show(){
+        return "I am a shape. My name is $this->name";
+    }
+}
